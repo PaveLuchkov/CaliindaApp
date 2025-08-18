ecd72d6f623a0d00b797be1b2c21079cdbd623ee
paveluckov <sliderlaad222@gmail.com>
2025-08-18 17:57:52 +0500

    Добавлены две фигуры для события входа Добавлено Preview

--- app/src/main/java/com/lpavs/caliinda/feature/calendar/presentation/components/events/cards/system/LogInEvent.kt
+++ app/src/main/java/com/lpavs/caliinda/feature/calendar/presentation/components/events/cards/system/LogInEvent.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,12 +21,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ExperimentalTextApi
@@ -33,6 +37,8 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
 import com.lpavs.caliinda.R
 import com.lpavs.caliinda.core.ui.theme.CalendarUiDefaults
@@ -61,6 +67,10 @@ fun LogInEvent(
     val cardBorderColor = colorScheme.onSurface
     val cardHeight = 100.dp
     val textStyle = Typography.headlineSmall
+    val onDialog = colorScheme.onTertiaryFixedVariant
+
+    val shape1 = MaterialShapes.Flower.toShape()
+    val shape2 = MaterialShapes.Cookie12Sided.toShape()
 
     Box(
         modifier =
@@ -81,10 +91,33 @@ fun LogInEvent(
                     shape = RoundedCornerShape(cuid.EventItemCornerRadius))
                 .background(cardBackground)
                 .height(cardHeight)) {
+        Box(
+            modifier =
+                Modifier.align(Alignment.BottomEnd)
+                    .size(100.dp)
+                    .offset(y = (10).dp, x = 10.dp)
+                    .rotate(75f)
+                    .border(
+                        width = 2.dp, color = onDialog.copy(alpha = 0.2f), shape = shape1)
+                    .clip(shape1)
+                    .background(onDialog.copy(alpha = 0f))
+        )
+
+        Box(
+            modifier =
+                Modifier
+                    .size(100.dp)
+                    .offset(y = (-10).dp, x = (-10).dp)
+                    .rotate(30f)
+                    .border(
+                        width = 2.dp, color = onDialog.copy(alpha = 0.2f), shape = shape2)
+                    .clip(shape2)
+                    .background(onDialog.copy(alpha = 0f))
+        )
         Column(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier =
-                Modifier.weight(1f) // Занимает все место, ОСТАВЛЯЯ место для кнопок снизу
+                Modifier.weight(1f)
                     .fillMaxWidth()
                     .padding(
                         horizontal = cuid.ItemHorizontalPadding,
@@ -118,4 +151,10 @@ fun LogInEvent(
             }
         }
     }
+}
+
+@Preview(showBackground = true, wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
+@Composable
+fun LogInEventPreview() {
+    LogInEvent(onSignInClick = {})
 }
