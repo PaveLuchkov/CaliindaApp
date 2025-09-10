324413d76a6d84d76e541191223bfeb679269888
paveluckov <sliderlaad222@gmail.com>
2025-09-10 12:02:58 +0500

    В AgentRecommendCard.kt: - Заменены параметры `title` и `description` на `suggestion` типа `Suggestion`. - Удален параметр `onSessionDelete` и добавлен `onConfirm`. - Обновлена логика отображения фона и цвета текста в зависимости от `suggestion.isRecommended`. - Добавлено отображение слотов из `suggestion.slots` с их названием, днем недели и временем. - В превью `AgentEventRecommendPreview` теперь используется объект `Suggestion` для отображения карточки. - Обновлены стили шрифтов и отступы.

--- app/src/main/java/com/lpavs/caliinda/feature/calendar/presentation/components/events/cards/agent/AgentRecommendCard.kt
+++ app/src/main/java/com/lpavs/caliinda/feature/calendar/presentation/components/events/cards/agent/AgentRecommendCard.kt
@@ -12,31 +12,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
-import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ExperimentalTextApi
@@ -49,16 +35,18 @@ import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lpavs.caliinda.R
+import com.lpavs.caliinda.core.data.remote.agent.Slot
+import com.lpavs.caliinda.core.data.remote.agent.Suggestion
+import com.lpavs.caliinda.core.data.remote.agent.Weekday
 import com.lpavs.caliinda.core.ui.theme.CalendarUiDefaults
 import com.lpavs.caliinda.core.ui.theme.cuid
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalTextApi::class)
 @Composable
 fun AgentRecommendItem(
-    title: String,
-    description: String,
+    suggestion: Suggestion,
     onToggleExpand: () -> Unit,
-    onSessionDelete: () -> Unit,
+    onConfirm: () -> Unit,
     isExpanded: Boolean,
 ) {
   val darkerShadowColor = Color.Black
@@ -69,22 +57,34 @@ fun AgentRecommendItem(
               R.font.robotoflex_variable,
               variationSettings =
                   FontVariation.Settings(
-                      FontVariation.weight(700),
+                      FontVariation.weight(600),
                       FontVariation.width(100f),
-                      FontVariation.opticalSizing(100.sp))))
-  val cardBackground = colorScheme.tertiaryFixed
-  val cardTextColor = colorScheme.onTertiaryFixedVariant
+                      FontVariation.opticalSizing(150.sp),
+                  )))
+  val slotNameFontFamily =
+      FontFamily(
+          Font(
+              R.font.robotoflex_variable,
+              variationSettings =
+                  FontVariation.Settings(
+                      FontVariation.weight(700),
+                      FontVariation.width(120f),
+                      FontVariation.opticalSizing(50.sp),
+                  )))
+  val isRecommened = suggestion.isRecommended
+  val cardBackground =
+      if (!isRecommened) colorScheme.secondaryContainer else colorScheme.primaryContainer
+  val cardTextColor =
+      if (!isRecommened) colorScheme.onSecondaryContainer else colorScheme.onPrimaryContainer
   val cardBorderColor = colorScheme.onTertiaryFixed
   val styleTitle = typography.headlineSmall
   val textTitleAlign = TextAlign.Start
-  val shape1 = MaterialShapes.SoftBurst.toShape()
-  val onDialog = colorScheme.onTertiaryFixedVariant
 
   Box(
       modifier =
           Modifier.padding(
                   horizontal = CalendarUiDefaults.ItemHorizontalPadding,
-                  vertical = CalendarUiDefaults.ItemVerticalPadding + 5.dp)
+                  vertical = CalendarUiDefaults.ItemVerticalPadding)
               .shadow(
                   elevation = cardElevation,
                   shape = RoundedCornerShape(cuid.EventItemCornerRadius),
@@ -98,16 +98,6 @@ fun AgentRecommendItem(
                   shape = RoundedCornerShape(cuid.EventItemCornerRadius))
               .background(cardBackground)
               .clickable(onClick = { onToggleExpand() })) {
-        val currentText = title
-//        Box(
-//            modifier =
-//                Modifier.align(Alignment.TopEnd)
-//                    .offset(y = (-10).dp, x = 10.dp)
-//                    .size(80.dp)
-//                    .rotate(75f)
-//                    .border(width = 2.dp, color = onDialog.copy(alpha = 0.2f), shape = shape1)
-//                    .clip(shape1)
-//                    .background(onDialog.copy(alpha = 0f)))
         Column {
           Box(
               modifier =
@@ -117,11 +107,12 @@ fun AgentRecommendItem(
                           vertical = cuid.AgentCardVerticalPadding),
               contentAlignment = Alignment.CenterStart) {
                 Text(
-                    text = currentText,
+                    text = suggestion.title,
                     color = cardTextColor,
                     textAlign = textTitleAlign,
                     style = styleTitle,
-                    fontFamily = cardFontFamily,)
+                    fontFamily = cardFontFamily,
+                )
               }
 
           AnimatedVisibility(
@@ -130,46 +121,74 @@ fun AgentRecommendItem(
                   fadeIn(animationSpec = tween(durationMillis = 150, delayMillis = 100)) +
                       expandVertically(
                           animationSpec = tween(durationMillis = 250, delayMillis = 50),
-                          expandFrom = Alignment.Top // Убрал .Companion для чистоты кода
-                          ),
+                          expandFrom = Alignment.Top),
               exit =
                   shrinkVertically(
-                      animationSpec = tween(durationMillis = 250),
-                      shrinkTowards = Alignment.Top // Убрал .Companion
-                      ) + fadeOut(animationSpec = tween(durationMillis = 150))) {
-                // Внутри AnimatedVisibility размещаем контент, который должен анимироваться
-                Box(
-                    modifier =
-                        Modifier.fillMaxWidth()
-                            .padding(
-                                horizontal = cuid.ItemHorizontalPadding,
-                                vertical = cuid.AgentCardVerticalPadding),
-                    contentAlignment = Alignment.CenterEnd) {
-                      val size = ButtonDefaults.ExtraSmallContainerHeight
-                      Button(
-                          onClick = { onSessionDelete() },
-                          modifier = Modifier.heightIn(size),
-                          contentPadding = ButtonDefaults.contentPaddingFor(size),
-                      ) {
-                        Icon(
-                            Icons.Filled.DeleteForever,
-                            contentDescription = "Localized description",
-                            modifier = Modifier.size(ButtonDefaults.iconSizeFor(size)),
-                        )
+                      animationSpec = tween(durationMillis = 250), shrinkTowards = Alignment.Top) +
+                      fadeOut(animationSpec = tween(durationMillis = 150))) {
+                Column {
+                  Box(
+                      modifier =
+                          Modifier.fillMaxWidth()
+                              .padding(
+                                  horizontal = cuid.ItemHorizontalPadding,
+                                  vertical = cuid.AgentCardVerticalPadding),
+                      contentAlignment = Alignment.CenterEnd) {
+                        Text(
+                            text = suggestion.description,
+                            style = typography.bodySmall,
+                            color = cardTextColor)
                       }
-                    }
+                  Column(
+                      modifier =
+                          Modifier.fillMaxWidth()
+                              .padding(
+                                  horizontal = cuid.ItemHorizontalPadding,
+                                  vertical = cuid.AgentCardVerticalPadding),
+                      horizontalAlignment = Alignment.CenterHorizontally) {
+                        suggestion.slots.forEach { slot ->
+                          Column(
+                              horizontalAlignment = Alignment.Start,
+                              modifier = Modifier.fillMaxWidth()) {
+                                Text(
+                                    text = slot.name,
+                                    style = typography.bodyLarge,
+                                    color = cardTextColor,
+                                    fontFamily = slotNameFontFamily)
+                                Text(
+                                    text = "${slot.day} • ${slot.startTime} - ${slot.endTime}",
+                                    style = typography.bodySmall,
+                                    color = cardTextColor)
+                              }
+                        }
+                        Button(onClick = {
+                            onConfirm
+                        }) { Text("Confirm plan") }
+                      }
+                }
               }
         }
       }
 }
 
-@Preview(showBackground = true, wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
+@Preview(showBackground = true, wallpaper = Wallpapers.YELLOW_DOMINATED_EXAMPLE)
 @Composable
 fun AgentEventRecommendPreview() {
-    AgentRecommendItem(
-      title = "Неподдерживаемый формат.",
-        description = "Описание",
-      isExpanded = false,
+  val fake_suggestion_not_rec =
+      Suggestion(
+          title = "Debug VS code",
+          description =
+              "When attempting to debug my app in Visual Studio Code using any bedugging platform, the process gets stuck in an infinite loading loop.",
+          slots =
+              listOf(
+                  Slot(Weekday.Monday, "Debugging tests", "07:00", "08:00"),
+                  Slot(Weekday.Wednesday, "Attemping to create outline", "17:30", "18:00"),
+                  Slot(Weekday.Friday, "Generate AI crap", "13:30", "14:30")),
+          isRecommended = true)
+  AgentRecommendItem(
+      suggestion = fake_suggestion_not_rec,
+      isExpanded = true,
       onToggleExpand = {},
-      onSessionDelete = {})
+      onConfirm = {}
+  )
 }
